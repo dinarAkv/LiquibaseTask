8caad4aa0af60517ca045439a5098f0e06685954
dinarAkv <dinarakv@yahoo.com>
2018-05-18 21:14:26 +0300

    Added 2nd report.

--- reports_script.sql
+++ reports_script.sql
@@ -1,11 +1,22 @@
 -- 1. Показать всех должников (чей товар доставлен, а сумма по договору
 -- не совпадает с суммой по платежам) по договорам с сортировкой по максимальному долгу.
-SELECT org.id, org.name, (ct.amount - pt.amount) AS Debt FROM organization org
+
+SELECT DISTINCT(org.id), org.name, (ct.amount - pt.amount) AS Debt FROM organization org
   INNER JOIN payment pt on org.id = pt.organization_id
   INNER JOIN contract ct on pt.contract_id = ct.id
-  INNER JOIN shipment sh on pt.shipment_id = sh.id
-WHERE sh.is_delivered = TRUE
-ORDER BY (ct.amount - pt.amount) DESC
+  INNER JOIN shipment_product shpr on pt.shipment_product_id = shpr.id
+  INNER JOIN shipment sh on shpr.shipment_id = sh.id
+WHERE sh.is_delivered = TRUE AND (ct.amount - pt.amount) > 0
+ORDER BY (ct.amount - pt.amount) DESC;
+
+
+
+-- 2. Показать самых лучших клиентов (принесших большую прибыль)
+
+SELECT org.id, org.name, SUM(pt.amount) AS profit FROM organization org
+  INNER JOIN payment pt ON org.id = pt.organization_id
+GROUP BY org.id
+ORDER BY profit DESC ;
 
 
 
